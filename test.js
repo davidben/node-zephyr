6ef4b6dd838e15d1ed66f73f7a900dafefe626c9
David Benjamin <davidben@mit.edu>
2013-04-24 15:19:21 -0400

    Tidy up test code some more
    Remove unnecessary console.dir at the start. Also register the notice listener
    earlier, so I can see the subscription SERVACKs.

--- test.js
+++ test.js
@@ -1,36 +1,38 @@
 var zephyr = require('./zephyr');
-console.dir(zephyr);
 
 var cls = process.argv[2];
 var inst = process.argv[3];
 
 zephyr.openPort();
+
+zephyr.on("notice", function(msg) {
+  if (msg.kind == zephyr.HMACK) {
+    console.log("HMACK : %s / %s / %s [%s]",
+		msg.class, msg.instance, msg.sender, msg.opcode);
+  } else if (msg.kind == zephyr.SERVACK) {
+    console.log("SERVACK : %s / %s / %s [%s] %s",
+		msg.class, msg.instance, msg.sender, msg.opcode,
+		msg.body[0]);
+  } else if (msg.kind == zephyr.SERVNAK) {
+    console.log("SERVNAK : %s / %s / %s [%s] %s",
+		msg.class, msg.instance, msg.sender, msg.opcode,
+		msg.body[0]);
+  } else {
+    console.log("%s / %s / %s %s [%s] (%s)\n%s",
+		msg.class, msg.instance, msg.sender,
+		(msg.checkedAuth == zephyr.ZAUTH_YES) ?
+		"AUTHENTIC" : "UNAUTHENTIC",
+		msg.opcode, msg.body[0], msg.body[1]);
+  }
+});
+
 zephyr.subscribeTo([ [ cls, inst, '*' ] ], function(err) {
   if (err) {
     console.dir(err);
     return;
   }
+  console.log('Subscribed to %s  %s', cls, inst);
 
-  zephyr.on("notice", function(msg) {
-    if (msg.kind == zephyr.HMACK) {
-      console.log("HMACK %s / %s / %s [%s]",
-		  msg.class, msg.instance, msg.sender, msg.opcode);
-    } else if (msg.kind == zephyr.SERVACK) {
-      console.log("SERVACK %s / %s / %s [%s] %s",
-		  msg.class, msg.instance, msg.sender, msg.opcode,
-		  msg.body[0]);
-    } else if (msg.kind == zephyr.SERVNAK) {
-      console.log("SERVNAK %s / %s / %s [%s] %s",
-		  msg.class, msg.instance, msg.sender, msg.opcode,
-		  msg.body[0]);
-    } else {
-      console.log("%s / %s / %s %s [%s] (%s)\n%s",
-		  msg.class, msg.instance, msg.sender,
-		  (msg.checkedAuth == zephyr.ZAUTH_YES) ?
-		  "AUTHENTIC" : "UNAUTHENTIC",
-		  msg.opcode, msg.body[0], msg.body[1]);
-    }
-  });
   process.stdin.on('data', function(message) {
     zephyr.sendNotice({
       class: cls,
